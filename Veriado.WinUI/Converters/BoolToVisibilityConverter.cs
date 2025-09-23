051aa7301e8b0effe4cefd847278852b522d3d90
AstraidLabs <46496187+AstraidLabs@users.noreply.github.com>
2025-09-23 22:21:22 +0200

    Add custom converters for visibility and item clicks

---
+++ Veriado.WinUI/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace Veriado.WinUI.Converters;
+
+public sealed class BoolToVisibilityConverter : IValueConverter
+{
+    public Visibility TrueValue { get; set; } = Visibility.Visible;
+
+    public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? TrueValue : FalseValue;
+        }
+
+        return FalseValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is Visibility visibility)
+        {
+            if (visibility == TrueValue)
+            {
+                return true;
+            }
+
+            if (visibility == FalseValue)
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
