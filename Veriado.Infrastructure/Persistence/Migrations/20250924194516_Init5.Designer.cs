c7b0ebb615d848957c57b89b74cc489c0aa839af
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-09-24 21:49:27 +0200

    fix

--- Veriado.Infrastructure/Persistence/Migrations/20250924194516_Init5.Designer.cs
+++ Veriado.Infrastructure/Persistence/Migrations/20250924194516_Init5.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Veriado.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using Veriado.Infrastructure.Persistence;
 namespace Veriado.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250924194516_Init5")]
+    partial class Init5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
