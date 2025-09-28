2014584113e4308dfca0de814010784919c9b811
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-09-28 21:34:18 +0200

    fix

--- Veriado.Mapping/DependencyInjection/MappingServiceCollectionExtensions.cs
+++ Veriado.Mapping/DependencyInjection/MappingServiceCollectionExtensions.cs
@@ -1,10 +1,10 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using AutoMapper;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Veriado.Appl.UseCases.Files.ApplySystemMetadata;
 using Veriado.Appl.UseCases.Files.ClearFileValidity;
 using Veriado.Appl.UseCases.Files.CreateFile;
@@ -25,8 +25,11 @@ public static class MappingServiceCollectionExtensions
     {
         ArgumentNullException.ThrowIfNull(services);
 
-            // Pokud používáš licencování v AM 15+, odkomentuj:
-            cfg.LicenseKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxNzkwNTUzNjAwIiwiaWF0IjoiMTc1OTA4MTEyNyIsImFjY291bnRfaWQiOiIwMTk5NTY2YTJlNDc3ODYxYjcwZGM0MzlhMjk4ODFkMyIsImN1c3RvbWVyX2lkIjoiY3RtXzAxazViNnF0NHR5ZDMxZ3N6eHJtemhnMnJ0Iiwic3ViX2lkIjoiLSIsImVkaXRpb24iOiIwIiwidHlwZSI6IjIifQ.AIwqOazIsxmdI-TtobavHD_HIU7xdRJI_oQZre1ATByMJuzDJfDRFGCyWkbDMvl8Vs6gWnTqqBj0Xuro4OlUpySdjjlIvbHJuSHSSY8-q4TfaPeMp1HIQmxw2hSJtKYa2STJ9a2D6yDKYe4xifM0OHrPhk0vxn63uCNWijfAdMEFm5LoMRHD7vnhDP-8R6RlqKt6c7tAJcKybRM0iRJ1MA94c__OSymob_kYwmE6xKWttPO2crigpEjRokenDjFymaFQKa-47KD70C3RvMQInuC2oaIfN-zIy_eq6NgMxB5sq4wZrR5HlKt8hGAncUt_90SluL_0rK7RWP9GhG74-A";
+        // AutoMapper konfigurace + registrace profilů
+        services.AddAutoMapper((sp, cfg) =>
+        {
+            // Pokud používáš licencování v AM 15+, odkomentuj a měj jistotu, že typ podporuje tento property:
+            // cfg.LicenseKey = "<tvůj_licenční_klíč>";
 
             CommonValueConverters.Register(cfg);
             cfg.AddProfile<FileReadProfiles>();
@@ -39,7 +42,7 @@ public static class MappingServiceCollectionExtensions
             new MapperValidationHostedService(
                 sp.GetRequiredService<AutoMapper.IConfigurationProvider>()));
 
-        // FluentValidation registrace
+        // FluentValidation registrace (explicitně – máš je už hotové)
         services.AddTransient<IValidator<CreateFileCommand>, CreateFileCommandValidator>();
         services.AddTransient<IValidator<ReplaceFileContentCommand>, ReplaceFileContentCommandValidator>();
         services.AddTransient<IValidator<UpdateFileMetadataCommand>, UpdateFileMetadataCommandValidator>();
@@ -49,7 +52,9 @@ public static class MappingServiceCollectionExtensions
         services.AddTransient<IValidator<ApplySystemMetadataCommand>, ApplySystemMetadataCommandValidator>();
         services.AddTransient<IValidator<SetFileReadOnlyCommand>, SetFileReadOnlyCommandValidator>();
 
+        // tvoje pipeline (ponechávám dle tvého projektu)
         services.AddTransient<WriteMappingPipeline>();
+
         return services;
     }
 
@@ -57,7 +62,8 @@ public static class MappingServiceCollectionExtensions
     {
         private readonly AutoMapper.IConfigurationProvider _config;
 
-        public MapperValidationHostedService(AutoMapper.IConfigurationProvider config) => _config = config;
+        public MapperValidationHostedService(AutoMapper.IConfigurationProvider config)
+            => _config = config;
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
