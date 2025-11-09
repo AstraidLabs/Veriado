2e2f572359a99252c114a72eac91c426cf62662f
AstraidLabs <46496187+AstraidLabs@users.noreply.github.com>
2025-11-09 14:50:41 +0100

    Make file detail dialog responsive

--- Veriado.WinUI/Views/Files/FileDetailDialog.xaml.cs
+++ Veriado.WinUI/Views/Files/FileDetailDialog.xaml.cs
@@ -1,18 +1,41 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Veriado.WinUI.Helpers;
 using Veriado.WinUI.ViewModels.Files;
 
 namespace Veriado.WinUI.Views.Files;
 
 public sealed partial class FileDetailDialog : ContentDialog
 {
+    private const double NarrowThreshold = 680.0;
+
     public FileDetailDialog()
     {
         InitializeComponent();
         PrimaryButtonClick += OnPrimaryButtonClick;
         SecondaryButtonClick += OnSecondaryButtonClick;
+        Loaded += OnLoaded;
+        LayoutRoot.SizeChanged += OnLayoutRootSizeChanged;
     }
 
     public FileDetailDialogViewModel? ViewModel => DataContext as FileDetailDialogViewModel;
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        ApplyWidthState();
+    }
+
+    private void OnLayoutRootSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        ApplyWidthState();
+    }
+
+    private void ApplyWidthState()
+    {
+        var width = ResponsiveHelper.GetEffectiveWidth(LayoutRoot);
+        var targetState = width < NarrowThreshold ? "Narrow" : "Wide";
+        _ = VisualStateManager.GoToState(this, targetState, true);
+    }
 
     private async void OnPrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
     {
