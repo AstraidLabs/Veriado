1be918b11edfe2e380c61e3b81139fe3c2e77ba6
AstraidLabs <46496187+AstraidLabs@users.noreply.github.com>
2025-11-09 09:53:31 +0100

    Refactor file validity badges to MVVM

--- Veriado.WinUI/Views/Files/FilesPage.xaml.cs
+++ Veriado.WinUI/Views/Files/FilesPage.xaml.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Threading;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
-using Veriado.Contracts.Files;
-using Veriado.WinUI.Helpers;
 using Veriado.WinUI.Services.Abstractions;
 using Veriado.WinUI.ViewModels.Files;
 
@@ -16,18 +12,15 @@ namespace Veriado.WinUI.Views.Files;
 public sealed partial class FilesPage : Page
 {
     private readonly IFilesSearchSuggestionsProvider _suggestionsProvider;
-    private readonly IServerClock _serverClock;
     private readonly DispatcherTimer _validityRefreshTimer;
     private CancellationTokenSource? _suggestionRequestSource;
 
     public FilesPage(
         FilesPageViewModel viewModel,
-        IFilesSearchSuggestionsProvider suggestionsProvider,
-        IServerClock serverClock)
+        IFilesSearchSuggestionsProvider suggestionsProvider)
     {
         ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
         _suggestionsProvider = suggestionsProvider ?? throw new ArgumentNullException(nameof(suggestionsProvider));
-        _serverClock = serverClock ?? throw new ArgumentNullException(nameof(serverClock));
         _validityRefreshTimer = new DispatcherTimer
         {
             Interval = TimeSpan.FromMinutes(1),
@@ -144,60 +137,6 @@ public sealed partial class FilesPage : Page
         source.Dispose();
     }
 
-    public Visibility GetValidityVisibility(FileSummaryDto? file)
-    {
-        return file?.Validity is null ? Visibility.Collapsed : Visibility.Visible;
-    }
-
-    public string GetValidityText(FileSummaryDto? file)
-    {
-        if (!FileValidityHelper.TryGetBadge(file?.Validity, _serverClock.NowLocal, out var badge))
-        {
-            return string.Empty;
-        }
-
-        return badge.Text;
-    }
-
-    public Brush GetValidityBackgroundBrush(FileSummaryDto? file)
-    {
-        var badge = FileValidityHelper.GetBadge(file?.Validity, _serverClock.NowLocal);
-        return badge.Background;
-    }
-
-    public Brush GetValidityForegroundBrush(FileSummaryDto? file)
-    {
-        var badge = FileValidityHelper.GetBadge(file?.Validity, _serverClock.NowLocal);
-        return badge.Foreground;
-    }
-
-    public string? GetValidityTooltip(FileSummaryDto? file)
-    {
-        if (file?.Validity is not { } validity)
-        {
-            return null;
-        }
-
-        var culture = CultureInfo.CurrentCulture;
-        var issued = validity.IssuedAt.ToLocalTime();
-        var validUntil = validity.ValidUntil.ToLocalTime();
-        return $"Platnost: {issued.ToString("d", culture)} – {validUntil.ToString("d", culture)}";
-    }
-
-    public bool TryGetDaysRemaining(FileSummaryDto? file, out int daysRemaining)
-    {
-        daysRemaining = 0;
-
-        if (FileValidityHelper.TryGetBadge(file?.Validity, _serverClock.NowLocal, out var badge)
-            && badge.DaysRemaining is { } remaining)
-        {
-            daysRemaining = remaining;
-            return true;
-        }
-
-        return false;
-    }
-
     private void OnValidityRefreshTimerTick(object? sender, object e)
     {
         UpdateValidityBadges();
@@ -205,6 +144,6 @@ public sealed partial class FilesPage : Page
 
     private void UpdateValidityBadges()
     {
-        Bindings?.Update();
+        ViewModel.RefreshValidityStates();
     }
 }
