24556d078be626cd89f411f869888547ca680713
AstraidLabs <46496187+AstraidLabs@users.noreply.github.com>
2025-11-30 19:08:05 +0100

    Disambiguate VTP type usage

--- Veriado.Infrastructure/Storage/Vpf/VpfPackageModels.cs
+++ Veriado.Infrastructure/Storage/Vpf/VpfPackageModels.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using VtpPackageInfo = Veriado.Application.Abstractions.VtpPackageInfo;
 
 namespace Veriado.Infrastructure.Storage.Vpf;
 
